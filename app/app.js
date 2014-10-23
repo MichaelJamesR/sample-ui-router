569cb22f9ebab99bf523057c1e2074c8d1d7066f
Mike <MichaelJamesR@users.noreply.github.com>
2014-10-23 14:15:04 -0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,48 +2,59 @@
 {
   var module = angular.module('myApp', ['ui.router']);
 
-  module.config(function($stateProvider, $urlRouterProvider)
+  module.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams)
   {
-    $urlRouterProvider.otherwise('nested-views-1');
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+  }]);
+
+  module.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider)
+  {
+    $urlRouterProvider
+      .otherwise('/');
     
     $stateProvider
-    .state('nested-views-1', {
-      url: "/nested-views-1",
-      templateUrl: "app/demos/nested-views-1/nested-views-1.tpl.html"
-    })
+      .state('/', {
+        templateUrl: "app/app.tpl.html"
+      })
+      
+      .state('nested-views-1', {
+        url: "/nested-views-1",
+        templateUrl: "app/demos/nested-views-1/nested-views-1.tpl.html"
+      })
+      
+      .state('nested-views-1.home', {
+        url: "/home",
+        templateUrl: "app/demos/nested-views-1/home/home.tpl.html"
+      })
+      
+      .state('nested-views-1.home.list', {
+        url: "/list",
+        templateUrl: "app/demos/nested-views-1/home/list/list.tpl.html",
+        controller: function($scope) 
+        {
+          $scope.items = ["A", "List", "Of", "Items"];
+        }
+      })
+      
+      .state('nested-views-1.about', {
+        url: "/about",
+        templateUrl: "app/demos/nested-views-1/about/about.tpl.html"
+      })
+      
+      .state('nested-views-1.about.details', {
+        url: "/details",
+        templateUrl: "app/demos/nested-views-1/about/details/details.tpl.html",
+        controller: function($scope) 
+        {
+          $scope.details = {
+            Name: 'Sample App',
+            Author: 'me',
+            Version: '0.0.1'
+          };
+        }
+      });
     
-    .state('nested-views-1.home', {
-      url: "/home",
-      templateUrl: "app/demos/nested-views-1/home/home.tpl.html"
-    })
-    
-    .state('nested-views-1.home.list', {
-      url: "/list",
-      templateUrl: "app/demos/nested-views-1/home/list/list.tpl.html",
-      controller: function($scope) 
-      {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    
-    .state('nested-views-1.about', {
-      url: "/about",
-      templateUrl: "app/demos/nested-views-1/about/about.tpl.html"
-    })
-    
-    .state('nested-views-1.about.details', {
-      url: "/details",
-      templateUrl: "app/demos/nested-views-1/about/details/details.tpl.html",
-      controller: function($scope) 
-      {
-        $scope.details = {
-          Name: 'Sample App',
-          Author: 'me',
-          Version: '0.0.1'
-        };
-      }
-    });
-    
-  });
+  }]);
 
 })();
